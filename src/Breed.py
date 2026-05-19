ea7d9adc0b5148e49ff354fa4cd809848a3b7880
Thom2503 <thomveldhuis03@gmail.com>
2026-05-19 21:24:17 +0200

    the breed class also needed some comments

--- src/Breed.py
+++ src/Breed.py
@@ -4,6 +4,10 @@ from Use import Use
 
 
 class Breed:
+    """
+    Breed class to have some more dynamic NPCs, with all kinds of different
+    mechanics and attributs. This class makes the Hierarchy wide not deep.
+    """
     name: str
     maxHealth: int
     attack: Attack
